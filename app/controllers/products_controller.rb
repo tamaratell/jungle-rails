5ebf0425b4247b9b634ce471ae1727166921fe82
Tamara Tell <2telltamara@gmail.com>
2023-07-04 15:32:17 -0400

    finish soldout feature

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -1,18 +1,10 @@
-Copy code
 class ProductsController < ApplicationController
   def index
     @products = Product.all.order(created_at: :desc)
-    @is_sold_out = sold_out_status(@products)
   end
 
   def show
     @product = Product.find(params[:id])
-    @is_sold_out = sold_out_status([@product])
   end
-
-  private
-
-  def sold_out_status(products)
-    products.map { |product| product.quantity.zero? }
-  end
+  
 end
