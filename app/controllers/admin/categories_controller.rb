4b6149822a095297b9bc4515e5ce5fa0da3e5bfe
Tamara Tell <2telltamara@gmail.com>
2023-07-04 22:15:37 -0400

    add admin/categories route

---
+++ app/controllers/admin/categories_controller.rb
@@ -0,0 +1,48 @@
+class Admin::CategoriesController < ApplicationController
+
+  before_action :authenticate_admin
+
+  def index
+    @products = Product.order(id: :desc).all
+  end
+
+  def new
+    @product = Product.new
+  end
+
+  def create
+    @product = Product.new(product_params)
+
+    if @product.save
+      redirect_to [:admin, :products], notice: 'Product created!'
+    else
+      render :new
+    end
+  end
+
+  def destroy
+    @product = Product.find params[:id]
+    @product.destroy
+    redirect_to [:admin, :products], notice: 'Product deleted!'
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(
+      :name,
+      :description,
+      :category_id,
+      :quantity,
+      :image,
+      :price
+    )
+  end
+
+  def authenticate_admin
+    authenticate_or_request_with_http_basic do |username, password|
+      username == ENV['USERNAME'] && password == ENV['PASSWORD']
+    end
+  end
+
+end
